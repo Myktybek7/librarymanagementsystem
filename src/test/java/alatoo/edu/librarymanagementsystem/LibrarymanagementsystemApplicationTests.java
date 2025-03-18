ba8ad60d6ce1ffb2a83172f80f38d2b210876022
Myktybek7 <myktybekabdykaiymov@gmail.com>
2025-03-18 18:05:05 +0600

    Initial commit: project setup

---
+++ src/test/java/alatoo/edu/librarymanagementsystem/LibrarymanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package alatoo.edu.librarymanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarymanagementsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
