ae3681c3dc409b008013f5dcde15ce60021f46ed
Myktybek7 <myktybekabdykaiymov@gmail.com>
2025-03-18 18:33:45 +0600

    Created REPO to interact with the database

---
+++ src/main/java/alatoo/edu/librarymanagementsystem/repository/PublisherRepository.java
@@ -0,0 +1,8 @@
+package alatoo.edu.librarymanagementsystem.repository;
+
+import com.alatoo.edu.librarymanagementsystem.entity.Publisher;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PublisherRepository extends JpaRepository<Publisher, Long> {
+
+}
